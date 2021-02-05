b50ce01a297c69a499d6047459dfb82c55fee481
Corbin Page <corbin.page@gmail.com>
2021-02-05 09:56:10 -0500

    Added waiting page

--- src/App.react.js
+++ src/App.react.js
@@ -74,7 +74,7 @@ function App(props: Props): React.Node {
             <Route exact path="/buy-ticket" component={InviteFriendPage} />
             <Route exact path="/deposits/:address" component={DepositsPage} />
             <Route exact path="/your-earnings/:address" component={YourEarningsSimple} />
-            {/* <Redirect to='/buy-ticket' /> */}
+            <Redirect to='/' />
             <Redirect from="/" to='/choo'/>
             <Route component={Error404} />
           </Switch>
